e1025f0f4bef969defbc42cfcca97869bf850b26
hsiaokuanglu <rraylu403@gmail.com>
2020-03-07 11:16:23 -0800

    Updates

---
+++ precache-manifest.137593ef12d918c0c42e6942708ae7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6493ac52cbf09d589aeeb279bec22402",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "72d5b193d03922a628d1",
+    "url": "/myapp/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "d550a644fde9dcbeb8bc",
+    "url": "/myapp/static/js/2.65908590.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/myapp/static/js/2.65908590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d5b193d03922a628d1",
+    "url": "/myapp/static/js/main.eeca6ec6.chunk.js"
+  },
+  {
+    "revision": "1716b5b1f163082744bb",
+    "url": "/myapp/static/js/runtime-main.aa379031.js"
+  }
+]);
